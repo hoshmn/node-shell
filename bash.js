343d8cbb9b660727eb846563342b26bb90ac4b2b
Joshua Maas-Howard <j.maas.howard@gmail.com>
2016-08-02 00:24:54 -0400

    che

--- bash.js
+++ bash.js
@@ -1,15 +1,25 @@
 var commands = require('./commands.js');
-
+//var request = require('request');
 
 process.stdout.write('prompt > ');
 // console.log(process.argv);
 process.stdin.on('data' ,function(data){
+
+  var self = commands.command;
+  var fn, args;
   var cmd = data.toString().trim();
-  cmd = cmd.split(' ');
-  var fn = cmd[0];
+  if (cmd.indexOf('|')>-1){
+  	var outs = cmd.split(/\s*\|\s*/g);
+  	args = outs.shift();
+  	args = args.split(' ');
+  	fn = args.shift();
+  	console.log('fn', fn, 'args', args, 'outs',outs);
+  	self[fn].call(self, args, outs);
+  } else {
 
-  var args = cmd.slice(1);
-
-  commands[fn].call(null, args);
-
+  args = cmd.split(' ');
+  fn = args.shift();
+  console.log(fn,args);
+  self[fn].call(self, args);
+  }
 });
--- commands.js
+++ commands.js
@@ -1,45 +1,62 @@
+'use strict';
+//can you bind  this" in a call to a required file?
+
+//must add check, if string treat differently than file
+
+//https://learn.fullstackacademy.com/
+//workshop/55bed1bf5fce400300b263b4/content/55bf45bba3130203003032dc/text
+//won't done still run before async finishes?
+
 var fs = require('fs');
 var request = require('request');
 
-function pwd(args){
-  process.stdout.write(process.env.PWD);
-  process.stdout.write('\nprompt > ');
-}
+var command = {
 
-function date(args){
-  var date1 = new Date(args);
-  process.stdout.write(date1.toString());
-  process.stdout.write('\nprompt > ');
-}
+ pwd: function(args){
+  var output = process.env.PWD;
 
-function ls(args){
+  command.done(output);
+},
+
+ date: function(args){
+  var date1 = new Date();
+  
+  var output = date1.toString();
+  command.done(output);
+},
+
+ ls: function(args){
+  var output = [];
   fs.readdir('.', function(err, files){
     if (err) throw err;
     files.forEach(function(file){
-      process.stdout.write(file.toString() + '\n');
+      output+=(file.toString() + '\n');
     })
-    process.stdout.write('\nprompt > ');
+    command.done(output);
   });
-}
+},
 
-function echo(args){
-  process.stdout.write(args.join(' '));
-  process.stdout.write('\nprompt > ');
-}
+ echo: function(args){
+  var output = args.join(' ');
+  command.done(output);
+},
 
-function cat(args){
+ cat: function(args, stdoutList){
   var output = '';
   args.forEach(function(file, index){
     fs.readFile(file,function(err,data){
       if (err) throw err;
       output += data + '\n';
-      }
+      if (index === args.length - 1) done(output, stdoutList);
+      //console.log('inside:',output);
     })
   });
-  done(output);
-}
+  //console.log('outside:'+output);
+  
+},
 
-function head(args){
+ head: function(args, stdoutList){
+  console.log('head called');
   var firstNLines = '';
   var output = '';
   args.forEach(function(file, index){
@@ -47,13 +64,13 @@ function head(args){
       if (err) throw err;
       firstNLines = data.toString().split('\n').slice(0,5).join('\n');
       output += firstNLines + '\n';
-      }
+      if (index === args.length - 1) command.done(output, stdoutList)
     })
   });
-  done(output);
-}
+  
+},
 
-function tail(args){
+ tail: function(args){
   var lastNLines = '';
   var dataLineArray
   var output = '';
@@ -62,27 +79,41 @@ function tail(args){
       if (err) throw err;
       dataLineArray = data.toString().split('\n');
       firstNLines = dataLineArray.slice(dataLineArray.length - 5).join('\n');
-      output += firstNLines + '\n';
+      output += firstNLines;
+      if (index === args.length - 1) {
+        command.done(output, stdoutList);
+      } else{ 
+        output += '\n\nnext file\n\n';
       }
     })
   });
-  done(output);
-}
+},
 
-function curl(args){
+curl: function(args){
   var url = args[0];
   request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
-      process.stdout.write(body);
+      var output = body;
     }
-    process.stdout.write('\nprompt > ');
+    command.done(output);
   })
+},
+
+ done: function(output,stdoutList){
+  if (stdoutList && stdoutList.length){
+    var fn = stdoutList.shift();
+    command[fn]([output], stdoutList);
+  } else {
+    process.stdout.write(output + '\nprompt > ');
+  }
 }
 
-function done(output){
-  process.stdout.write(output + '\nprompt > ');
-}
+};
 
+
+module.exports.command = command;
+
+/*
 module.exports.pwd = pwd;
 module.exports.date = date;
 module.exports.ls = ls;
@@ -90,4 +121,5 @@ module.exports.echo = echo;
 module.exports.cat = cat;
 module.exports.head = head;
 module.exports.tail = tail;
-module.exports.curl = curl;
+*/
+//module.exports.curl = curl;
